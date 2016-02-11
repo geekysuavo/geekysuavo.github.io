a3ebd5a8a48b6bf326a6cc694d2ee189a923f810
Bradley Worley <geekysuavo@gmail.com>
2016-02-11 12:37:31 -0600

    fixed sidebar string handling

--- res/wrap/wrap.js
+++ res/wrap/wrap.js
@@ -5,7 +5,7 @@ var subtotal = 0.0;
 /* setInnerHTML: sets the inner html of an element. */
 function setInnerHTML (id, markup) {
   /* get the element. */
-  var el = document.getElementById (id);
+  var el = document.getElementById(id);
 
   /* set the inner html. */
   el.innerHTML = markup;
@@ -14,7 +14,7 @@ function setInnerHTML (id, markup) {
 /* setPosition: sets the position of an HTML element. */
 function setPosition (id, x, y) {
   /* get the element. */
-  var el = document.getElementById (id);
+  var el = document.getElementById(id);
 
   /* set the position of the element. */
   el.style.left = x + "px";
@@ -24,7 +24,7 @@ function setPosition (id, x, y) {
 /* setWidth: sets the width of an HTML element. */
 function setWidth (id, w) {
   /* get the element. */
-  var el = document.getElementById (id);
+  var el = document.getElementById(id);
 
   /* set the wdith of the element. */
   el.style.width = w + "px";
@@ -33,7 +33,7 @@ function setWidth (id, w) {
 /* setHeight: sets the height of an HTML element. */
 function setHeight (id, h) {
   /* get the element. */
-  var el = document.getElementById (id);
+  var el = document.getElementById(id);
 
   /* set the wdith of the element. */
   el.style.height = h + "px";
@@ -42,7 +42,7 @@ function setHeight (id, h) {
 /* setDimension: sets the size of an HTML element. */
 function setDimension (id, w, h) {
   /* get the element. */
-  var el = document.getElementById (id);
+  var el = document.getElementById(id);
 
   /* set the size of the element. */
   el.style.width = w + "px";
@@ -52,7 +52,7 @@ function setDimension (id, w, h) {
 /* setSrc: sets the src attribute of an HTML element. */
 function setSrc (id, src) {
   /* get the element. */
-  var el = document.getElementById (id);
+  var el = document.getElementById(id);
 
   /* set the wdith of the element. */
   el.src = src;
@@ -61,7 +61,7 @@ function setSrc (id, src) {
 /* showElement: sets an element as visible. */
 function showElement (id) {
   /* get the element. */
-  var el = document.getElementById (id);
+  var el = document.getElementById(id);
 
   /* show the element. */
   el.style.display = "block";
@@ -70,7 +70,7 @@ function showElement (id) {
 /* hideElement: sets an element as invisible. */
 function hideElement (id) {
   /* get the element. */
-  var el = document.getElementById (id);
+  var el = document.getElementById(id);
 
   /* hide the element. */
   el.style.display = "none";
@@ -79,45 +79,45 @@ function hideElement (id) {
 /* showLightbox: initializes and shows the lightbox with a given image. */
 function showLightbox (project, filename, url, width, height, tip) {
   /* set the lightbox image size. */
-  setDimension ("lightboxImage", width, height);
+  setDimension("lightboxImage", width, height);
 
   /* calculate the lightbox image coordinates. */
   var x = window.innerWidth / 2.0 - (width / 2.0);
   var y = window.innerHeight / 2.0 - (height / 2.0);
 
   /* set the lightbox image coordinates. */
-  setPosition ("lightboxImage", x, y);
+  setPosition("lightboxImage", x, y);
 
   /* set the lightbox title coordinates. */
-  setPosition ("lightboxTitle", x + 20, y + 20);
+  setPosition("lightboxTitle", x + 20, y + 20);
 
   /* set the lightbox text coordinates and width. */
-  setPosition ("lightboxText", x, y + height);
-  setWidth ("lightboxText", width);
+  setPosition("lightboxText", x, y + height);
+  setWidth("lightboxText", width);
 
   /* set the lightbox image source. */
-  setSrc ("lightboxImage", url);
+  setSrc("lightboxImage", url);
 
   /* set the lightbox title text string. */
-  setInnerHTML ("lightboxTitle", filename);
+  setInnerHTML("lightboxTitle", filename);
 
   /* set the lightbox text strings. */
-  setInnerHTML ("lightboxText", tip);
+  setInnerHTML("lightboxText", tip);
 
   /* show the lightbox elements. */
-  showElement ("lightboxDiv");
-  showElement ("lightboxText");
-  showElement ("lightboxTitle");
-  showElement ("lightboxImage");
+  showElement("lightboxDiv");
+  showElement("lightboxText");
+  showElement("lightboxTitle");
+  showElement("lightboxImage");
 }
 
 /* hideLightbox: hides the lightbox. */
 function hideLightbox () {
   /* hide the lightbox elements. */
-  hideElement ("lightboxDiv");
-  hideElement ("lightboxText");
-  hideElement ("lightboxTitle");
-  hideElement ("lightboxImage");
+  hideElement("lightboxDiv");
+  hideElement("lightboxText");
+  hideElement("lightboxTitle");
+  hideElement("lightboxImage");
 }
 
 /* scrollToId: scroll the page to a given DOM element. */
@@ -130,11 +130,11 @@ function scrollToId (idStr) {
 /* completeSidebar: complete the page sidebar, if it exists. */
 function completeSidebar () {
   /* check if the sidebar exists. */
-  var sb = document.getElementById ("sidebar");
+  var sb = document.getElementById("sidebar");
   if (!sb) { return; }
 
   /* look up all <h1> tags in the document. */
-  var heads = document.getElementsByTagName ("h1");
+  var heads = document.getElementsByTagName("h1");
   if (!heads) { return; }
 
   /* initialize the sidebar html content string. */
@@ -143,7 +143,7 @@ function completeSidebar () {
   /* loop over each tag. */
   for (var i = 0; i < heads.length; i++) {
     /* build a link string from the tag id. */
-    var headId = heads[i].id.replace ("-", " ");
+    var headId = heads[i].id.split('-').join(' ');
     var headStr = '<a href="#" onclick="scrollToId(\'' +
                   headId + '\'); return false;">' +
                   '<p class="seclink">' + headId + '</p></a>';
@@ -153,8 +153,8 @@ function completeSidebar () {
   }
 
   /* update the contents of the sidebar div and make it visible. */
-  setInnerHTML ("sidebar", str);
-  showElement ("sidebar");
+  setInnerHTML("sidebar", str);
+  showElement("sidebar");
 }
 
 /* completeTables: complete all bills of materials, if any exist. */
@@ -187,6 +187,6 @@ function completeTables () {
 
 /* window.onload: perform intelligent page content completion. */
 window.onload = function () {
-  completeSidebar ();
-  completeTables ();
+  completeSidebar();
+  completeTables();
 };
